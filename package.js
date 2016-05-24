83c623b92533e9ac516d6d0188622079ab628d14
Paulo Diniz <pdiniz_100@yahoo.com>
2016-05-24 12:06:57 -0700

    Intial commit as new package.

---
+++ package.js
@@ -0,0 +1,22 @@
+//I've submitted a PR to the base branch with our updates. Hopefully he'll merge those in soon and we'll be able to drop this branch.
+Package.describe({
+    name: 'risul:moment-timezone',
+    summary: 'Timezone support for moment.js, packaged for Meteor',
+    version: '0.5.4',
+    git: 'https://github.com/risul/meteor-moment-timezone'
+});
+
+Package.on_use(function (api, where) {
+    var where = where || ['client', 'server'];
+    var momentPackageName = !!Package.onUse ? "momentjs:moment@2.13.1" : "moment";
+    
+    api.use(momentPackageName, where);
+    
+    if (api.imply) {
+        api.imply(momentPackageName, where);
+    }
+    
+    api.add_files('preFix.js', where);
+    api.add_files('lib/moment-timezone-with-data.js', where);
+    api.add_files('postFix.js', where);
+});
